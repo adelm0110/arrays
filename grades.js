2bda245e574f8941777448c677e21aabbaee11ec
Adel Matias <adelmatias110@gmail.com>
2023-12-07 16:45:55 -0500

    changes

--- grades.js
+++ grades.js
@@ -1,10 +1,23 @@
 document
 .querySelector('form')
-.addEventListener('submit', setGrade)
+.addEventListener('submit',  setGrade)
 
 let grades = [];
 function setGrade(e) {
     e.preventDefault();
-    let grade = document.querySelector('#grade').value
-    grades.push(grade);
+    let grade = parseFloat(document.querySelector('#grade').value)
+    let name = document.querySelector('#name').value
+    console.log(grades);
+    grades.push({name,grade});
+    printGrades();
+}
+
+function printGrades() {
+    let o = document.querySelector('#output');
+    let sum = 0;
+    for (let i=0; i<grades.length; i++) {
+        sum += sum/grades.length;
+    }
+    let avg = sum / grades.length;
+    o.innerText = avg;
 }
